7b890dcf0e92a664e6704b5c1328fc5e88db8cba
TaeHoon <31303655+xoasml@users.noreply.github.com>
2024-07-30 18:10:10 +0900

    REST API

--- src/main/java/com/hoon/api/domain/example/ExampleController.java
+++ src/main/java/com/hoon/api/domain/example/ExampleController.java
@@ -6,25 +6,44 @@ import com.hoon.api.utils.response.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/example")
 @RequiredArgsConstructor
 public class ExampleController {
 
-    private final ExampleService exampleService;
-
-    @PostMapping("/save")
+    @GetMapping("")
     @MethodInfoLogging(description = "예제 메소드 저장 API")
     @Operation(summary = "예제 메소드 저장 API", description = "예제 메소드 저장 API 설명")
-    public CommonResponse<?> save(@Valid ExampleSaveRequestDto request){
+    public CommonResponse<?> get(@Valid ExampleSaveRequestDto request){
 
-        exampleService.save(request);
+        return new CommonResponse<>("리스트 조회 성공");
+    }
+
+    @PostMapping("")
+    @MethodInfoLogging(description = "예제 메소드 저장 API")
+    @Operation(summary = "예제 메소드 저장 API", description = "예제 메소드 저장 API 설명")
+    public CommonResponse<?> post(@Valid ExampleSaveRequestDto request){
 
         return new CommonResponse<>("저장 성공");
     }
+
+    @PutMapping("/{pk}")
+    @MethodInfoLogging(description = "예제 수정 API")
+    @Operation(summary = "예제 메소드 저장 API", description = "예제 메소드 저장 API 설명")
+    public CommonResponse<?> put(@Valid ExampleSaveRequestDto request, @RequestParam Integer pk){
+
+        return new CommonResponse<>("수정 성공");
+    }
+
+    @DeleteMapping("/{pk}")
+    @MethodInfoLogging(description = "예제 메소드 삭제 (DELETE) API")
+    @Operation(summary = "예제 메소드 저장 API", description = "예제 메소드 저장 API 설명")
+    public CommonResponse<?> delete(@Valid ExampleSaveRequestDto request, @RequestParam Integer pk){
+
+        return new CommonResponse<>("삭제 성공");
+    }
+
 
 }
--- src/main/java/com/hoon/api/domain/redis/RedisController.java
+++ src/main/java/com/hoon/api/domain/redis/RedisController.java
@@ -1,6 +1,7 @@
 package com.hoon.api.domain.redis;
 
 import com.hoon.api.domain.redis.dto.RedisSetKeyValueRequestDto;
+import com.hoon.api.utils.aop.annotation.MethodInfoLogging;
 import com.hoon.api.utils.redis.RedisRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +15,13 @@ public class RedisController {
     private final RedisRepository redisRepository;
 
     @PostMapping("/set")
+    @MethodInfoLogging(description = "Redis에 Key, Value 저장")
     public void setKeyValue(@Valid @RequestBody RedisSetKeyValueRequestDto request) {
         redisRepository.setValue(request.getKey(), request.getValue());
     }
 
     @GetMapping("/get/{key}")
+    @MethodInfoLogging(description = "Redis에 Key로 Value 조회")
     public String getKeyValue(@PathVariable String key) {
         return redisRepository.getValue(key);
     }
